9ceda55f9128e92b2deeefb98a52a78cbe17386f
Jack-Hunter14 <123279956+NES-Rovys@users.noreply.github.com>
2024-06-03 21:35:21 +1000

    HELP

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 var page = "videos";
 var dropdown = "";
+var dropPos = []
 
 function newPage(id) {
-    document.getElementById(page).style.display = "none";
-    document.getElementById(id).style.display = "block";
-    page = id;
+  document.getElementById(page).style.display = "none";
+  document.getElementById(id).style.display = "block";
+  page = id;
 }
 
-function showMenu(id) {;
-  switch(id) {
-    case "dropVid": 
-      id = "downVid";
-      break;
-    case "dropQuo":
-      id = "downQuo";
-      break;
-    case "dropGam":
-      id = "downGam";
-      break;
-    case "dropShr":
-      id = "downShr";
-      break;
-  }
+function showMenu(id) {
+  id = "down" + id.slice(4);
   if (dropdown != "") {
     document.getElementById(dropdown).classList.toggle("show");
   }  
-  document.getElementById(id).classList.toggle("show");
-  dropdown = id;
+  if (dropdown != id) {
+    document.getElementById(id).classList.toggle("show");
+    dropdown = id;
+  } else {
+    dropdown = "";
+  }
+}
+
+function goToPage(id) {
+  switch(id.slice(0, 3)) {
+    case "Vid":
+      newPage("videos");
+      break;
+    case "Quo":
+      newPage("quotes");
+      break;
+    case "Gam":
+      newPage("gaming");
+      break;
+    case "Shr":
+      newPage("shrine");
+      break;
+  }
+}
+
+window.onclick = function(event) {
+  console.log(!event.target.matches('.arrowImage'));
+  if (!event.target.matches('.arrowImage')) {
+    for (let i = 0; i < document.querySelectorAll('.menu').length; i++) {
+      if (document.querySelectorAll('.menu')[i].classList.contains('show')) {
+        document.querySelectorAll('.menu')[i].classList.remove('show');
+        dropdown = "";
+      }
+    }
+  }
 }
